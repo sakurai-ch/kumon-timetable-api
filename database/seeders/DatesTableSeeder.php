dace615aaacfb66a3a1c6c31fbdaf833b61bf4d9
sakurai <sakurai_ch@yahoo.co.jp>
2021-06-16 22:08:12 +0900

    first commit

--- database/seeders/DatesTableSeeder.php
+++ database/seeders/DatesTableSeeder.php
@@ -14,15 +14,13 @@ class DatesTableSeeder extends Seeder
      */
     public function run()
     {
-        Date::insert(['id' => 1, 'date' => '3/1', 'day' => '(月)', 'order' => 1]);
-        Date::insert(['id' => 2, 'date' => '3/2', 'day' => '(火)', 'order' => 2]);
-        Date::insert(['id' => 3, 'date' => '3/3', 'day' => '(水)', 'order' => 3]);
-        Date::insert(['id' => 4, 'date' => '3/4', 'day' => '(木)', 'order' => 4]);
-        Date::insert(['id' => 5, 'date' => '3/5', 'day' => '(金)', 'order' => 5]);
-        Date::insert(['id' => 6, 'date' => '3/6', 'day' => '(土)', 'order' => 6]);
-        Date::insert(['id' => 7, 'date' => '3/7', 'day' => '(日)', 'order' => 7]);
-        Date::insert(['id' => 8, 'date' => '3/8', 'day' => '(月)', 'order' => 8]);
-        Date::insert(['id' => 9, 'date' => '3/9', 'day' => '(火)', 'order' => 9]);
-        Date::insert(['id' => 10, 'date' => '3/10', 'day' => '(水)', 'order' => 10]);
+        Date::insert(['id' => 1, 'date' => '6/29', 'day' => '(火)', 'order' => 1]);
+        Date::insert(['id' => 2, 'date' => '6/30', 'day' => '(水)', 'order' => 2]);
+        Date::insert(['id' => 3, 'date' => '7/2', 'day' => '(金)', 'order' => 3]);
+        Date::insert(['id' => 4, 'date' => '7/6', 'day' => '(火)', 'order' => 4]);
+        Date::insert(['id' => 5, 'date' => '7/7', 'day' => '(水)', 'order' => 5]);
+        Date::insert(['id' => 6, 'date' => '7/9', 'day' => '(金)', 'order' => 6]);
+        Date::insert(['id' => 7, 'date' => '7/13', 'day' => '(火)', 'order' => 7]);
+        Date::insert(['id' => 8, 'date' => '7/14', 'day' => '(水)', 'order' => 8]);
     }
 }
--- database/seeders/TimeRequestsTableSeeder.php
+++ database/seeders/TimeRequestsTableSeeder.php
@@ -24,8 +24,8 @@ class TimeRequestsTableSeeder extends Seeder
         TimeRequest::insert(['user_id' => '2', 'frame_id' => '16', 'state' => '']);
         TimeRequest::insert(['user_id' => '2', 'frame_id' => '26', 'state' => '']);
         TimeRequest::insert(['user_id' => '2', 'frame_id' => '46', 'state' => '']);
-        TimeRequest::insert(['user_id' => '2', 'frame_id' => '86', 'state' => '']);
-        TimeRequest::insert(['user_id' => '2', 'frame_id' => '96', 'state' => '']);
+        TimeRequest::insert(['user_id' => '2', 'frame_id' => '56', 'state' => '']);
+        TimeRequest::insert(['user_id' => '2', 'frame_id' => '66', 'state' => '']);
 
         TimeRequest::insert(['user_id' => '3', 'frame_id' => '26', 'state' => '']);
 
@@ -37,12 +37,12 @@ class TimeRequestsTableSeeder extends Seeder
         TimeRequest::insert(['user_id' => '4', 'frame_id' => '26', 'state' => '']);
         TimeRequest::insert(['user_id' => '4', 'frame_id' => '27', 'state' => '']);
         TimeRequest::insert(['user_id' => '4', 'frame_id' => '28', 'state' => '']);
-        TimeRequest::insert(['user_id' => '4', 'frame_id' => '29', 'state' => '']);
-        TimeRequest::insert(['user_id' => '4', 'frame_id' => '30', 'state' => '']);
+        TimeRequest::insert(['user_id' => '4', 'frame_id' => '31', 'state' => '']);
+        TimeRequest::insert(['user_id' => '4', 'frame_id' => '32', 'state' => '']);
 
-        TimeRequest::insert(['user_id' => '5', 'frame_id' => '20', 'state' => '']);
         TimeRequest::insert(['user_id' => '5', 'frame_id' => '30', 'state' => '']);
-        TimeRequest::insert(['user_id' => '5', 'frame_id' => '50', 'state' => '']);
+        TimeRequest::insert(['user_id' => '5', 'frame_id' => '40', 'state' => '']);
+        TimeRequest::insert(['user_id' => '5', 'frame_id' => '60', 'state' => '']);
         
         TimeRequest::insert(['user_id' => '6', 'frame_id' => '1', 'state' => '']);
         TimeRequest::insert(['user_id' => '6', 'frame_id' => '2', 'state' => '']);
@@ -51,38 +51,38 @@ class TimeRequestsTableSeeder extends Seeder
         TimeRequest::insert(['user_id' => '6', 'frame_id' => '12', 'state' => '']);
         TimeRequest::insert(['user_id' => '6', 'frame_id' => '13', 'state' => '']);
         
-        TimeRequest::insert(['user_id' => '7', 'frame_id' => '86', 'state' => '']);
-        TimeRequest::insert(['user_id' => '7', 'frame_id' => '87', 'state' => '']);
+        TimeRequest::insert(['user_id' => '7', 'frame_id' => '46', 'state' => '']);
+        TimeRequest::insert(['user_id' => '7', 'frame_id' => '47', 'state' => '']);
 
         TimeRequest::insert(['user_id' => '8', 'frame_id' => '25', 'state' => '']);
 
         TimeRequest::insert(['user_id' => '9', 'frame_id' => '25', 'state' => '']);
 
-        TimeRequest::insert(['user_id' => '10', 'frame_id' => '85', 'state' => '']);
-        TimeRequest::insert(['user_id' => '10', 'frame_id' => '86', 'state' => '']);
-        TimeRequest::insert(['user_id' => '10', 'frame_id' => '87', 'state' => '']);
+        TimeRequest::insert(['user_id' => '10', 'frame_id' => '55', 'state' => '']);
+        TimeRequest::insert(['user_id' => '10', 'frame_id' => '56', 'state' => '']);
+        TimeRequest::insert(['user_id' => '10', 'frame_id' => '57', 'state' => '']);
         
         TimeRequest::insert(['user_id' => '11', 'frame_id' => '45', 'state' => '']);
         TimeRequest::insert(['user_id' => '11', 'frame_id' => '46', 'state' => '']);
         TimeRequest::insert(['user_id' => '11', 'frame_id' => '48', 'state' => '']);
-        TimeRequest::insert(['user_id' => '11', 'frame_id' => '49', 'state' => '']);
-        TimeRequest::insert(['user_id' => '11', 'frame_id' => '50', 'state' => '']);
+        TimeRequest::insert(['user_id' => '11', 'frame_id' => '61', 'state' => '']);
+        TimeRequest::insert(['user_id' => '11', 'frame_id' => '59', 'state' => '']);
         
         TimeRequest::insert(['user_id' => '12', 'frame_id' => '15', 'state' => '']);
         
-        TimeRequest::insert(['user_id' => '13', 'frame_id' => '20', 'state' => '']);
+        TimeRequest::insert(['user_id' => '13', 'frame_id' => '29', 'state' => '']);
+        TimeRequest::insert(['user_id' => '13', 'frame_id' => '39', 'state' => '']);
+        TimeRequest::insert(['user_id' => '13', 'frame_id' => '59', 'state' => '']);
         TimeRequest::insert(['user_id' => '13', 'frame_id' => '30', 'state' => '']);
-        TimeRequest::insert(['user_id' => '13', 'frame_id' => '50', 'state' => '']);
-        TimeRequest::insert(['user_id' => '13', 'frame_id' => '90', 'state' => '']);
-        TimeRequest::insert(['user_id' => '13', 'frame_id' => '100', 'state' => '']);
+        TimeRequest::insert(['user_id' => '13', 'frame_id' => '40', 'state' => '']);
         
-        TimeRequest::insert(['user_id' => '14', 'frame_id' => '20', 'state' => '']);
+        TimeRequest::insert(['user_id' => '14', 'frame_id' => '29', 'state' => '']);
         
         TimeRequest::insert(['user_id' => '15', 'frame_id' => '46', 'state' => '']);
         TimeRequest::insert(['user_id' => '15', 'frame_id' => '47', 'state' => '']);
 
-        TimeRequest::insert(['user_id' => '16', 'frame_id' => '85', 'state' => '']);
-        TimeRequest::insert(['user_id' => '16', 'frame_id' => '86', 'state' => '']);
+        TimeRequest::insert(['user_id' => '16', 'frame_id' => '75', 'state' => '']);
+        TimeRequest::insert(['user_id' => '16', 'frame_id' => '76', 'state' => '']);
         
         TimeRequest::insert(['user_id' => '17', 'frame_id' => '44', 'state' => '']);
         
@@ -94,6 +94,6 @@ class TimeRequestsTableSeeder extends Seeder
         TimeRequest::insert(['user_id' => '19', 'frame_id' => '2', 'state' => '']);
         TimeRequest::insert(['user_id' => '19', 'frame_id' => '3', 'state' => '']);
         
-        TimeRequest::insert(['user_id' => '20', 'frame_id' => '100', 'state' => '']);
+        TimeRequest::insert(['user_id' => '20', 'frame_id' => '9', 'state' => '']);
     }
 }
